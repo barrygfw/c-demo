bd47d674e200a49091c287d137f6649691f0c032
barry_g <barry_g@163.com>
2018-11-30 13:09:32 +0800

    添加子段区间解

--- MaxSubSum.c
+++ MaxSubSum.c
@@ -2,7 +2,7 @@
 * @Author: barry_g
 * @Date:   2018-10-12 10:49:10
 * @Last Modified by:   barry_g
-* @Last Modified time: 2018-10-12 14:48:40
+* @Last Modified time: 2018-11-30 12:34:52
 */
 /**
  * 最大子段和
@@ -10,6 +10,9 @@
 #include <stdio.h>
 #define MAX 100
 int a[MAX];
+int L=0;
+int R=0;
+int answer=0;
 /**
  * [maxsub 最大子段和函数]
  * @param  a     [保存数字的数组]
@@ -18,7 +21,7 @@ int a[MAX];
  * @return       [最大子段和]
  */
 int maxsub(int *a,int left,int right){
-	int i;
+	int i,l,r;
 	int center;
 	int sum,left_sum=0,right_sum=0;//left_sum左子段最大子段和、right_sum右子段最大子段和
 	int left_max=0,right_max=0;//left_max中间往左最大子段和、right_max中间往右最大子段和
@@ -35,24 +38,34 @@ int maxsub(int *a,int left,int right){
 		right_sum = maxsub(a,center+1,right);
 		//求中间往左最大子段和
 		sum = 0;
+		l=center;
 		for(i=center;i>=left;i--){
 			sum+=a[i];
-			if(sum>left_max)
+			if(sum>left_max){
+				l=i;
 				left_max = sum;
+			}
 		}
 		//求中间往右最大子段和
 		sum=0;
+		r=center;
 		for(i=center+1;i<=right;i++){
 			sum+=a[i];
-			if(sum>right_max)
+			if(sum>right_max){
+				r=i;
 				right_max = sum;
+			}
 		}
 		sum = left_max+right_max;
 		//判断最大子段和是在中间还是在两边
-		if(sum<left_sum)
+		if(sum<left_sum){
 			sum = left_sum;
+		}
 		if(sum<right_sum)
 			sum = right+sum;
+		if(sum>answer){
+			answer=sum;L=l;R=r;
+		}
 	}
 	return sum;
 }
@@ -65,6 +78,7 @@ int main()
 	for(i=0;i<count;i++){
 		scanf("%d",&a[i]);
 	}
-	printf("the MaxSubNum is:%d\n", maxsub(a,0,count-1));
+	maxsub(a,0,count-1);
+	printf("the MaxSubNum is:%d\nthe answer index of (%d,%d)",answer,L,R);
 	return 0;
 }
